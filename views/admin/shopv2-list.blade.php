3d6915db49cdd8203c557eb722316671343746d9
unknown <game.oh.vn@gmail.com>
2024-02-15 13:35:10 +0700

    f

--- views/admin/shopv2-list.blade.php
+++ views/admin/shopv2-list.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
 
+<a href="{{ route('ecmin.shopv2.order.find') }}">Manage order</a>
+
 @foreach ($shops as $shop)
 	<a href="{{ route('ecmin.shopv2.open', ['slug' => $shop->slug]) }}">Open {{ $shop->name }}</a><br/>
 @endforeach 
